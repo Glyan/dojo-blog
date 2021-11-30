20af8c234024a49cafff22d2cd8b07dc1c08f10c
Glyan <pamela27jr@gmail.com>
2021-11-30 21:51:51 +0000

    Functions as props

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,13 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+    }
+
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
         </div>
     );
 }
