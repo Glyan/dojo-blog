b364953c122c2d2226a1255040dc17f8ec0e3f82
Glyan <pamela27jr@gmail.com>
2021-11-30 21:10:09 +0000

    Trying out buttons and events

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = "http://www.google.com";
-
   return (
     <div className="App">
       <Navbar />
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello World', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello ' + name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('Glyan', e)}>Click me again</button>
         </div>
     );
 }
